12e1f2ff34da757edb325d7e9520eb0adc2ea734
Hiroaki SHIBUKI <Hiroaki.SHIBUKI@nifty.com>
2014-12-30 11:34:06 +0900

    Add StringFactory class and its test.

---
+++ Mjollnir.Shared.Tests/Text/StringFactoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Mjollnir.Shared.Text.Test
+{
+    [TestClass]
+    public class StringFactoryTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            AssertEx.Throws<ArgumentNullException>(() => StringFactory.Create(null));
+
+            StringFactory.Create(sb =>
+            {
+                sb.Append("A");
+                sb.Append("B");
+                sb.Append("C");
+            })
+            .Is("ABC");
+        }
+    }
+}

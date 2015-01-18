91cac436403309ec09ff14a2a63542fb3d7daa23
Hiroaki SHIBUKI <Hiroaki.SHIBUKI@nifty.com>
2015-01-18 09:24:09 +0900

    Addred ByteArrayFactory

---
+++ Mjollnir.Tests/ByteArrayFactoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mjollnir
+{
+    [TestClass]
+    public class ByteArrayFactoryTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var action = ((Action<Stream>)(stream => 
+            {
+                stream.WriteByte(1);
+                stream.WriteByte(2);
+                stream.WriteByte(3);
+            }));
+
+            AssertEx.Throws<ArgumentNullException>(() => ByteArrayFactory.Create(null));
+
+            var bytes = ByteArrayFactory.Create(action);
+
+            bytes[0].Is((byte)1);
+            bytes[1].Is((byte)2);
+            bytes[2].Is((byte)3);
+        }
+    }
+}

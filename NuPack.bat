9b60dab1741b3ff6de25c82e5dc70dc6b6ac2e93
Hiroaki SHIBUKI <Hiroaki.SHIBUKI@nifty.com>
2015-01-18 12:03:47 +0900

    Improved NuPack.bat and NuPush.bat

--- NuPack.bat
+++ NuPack.bat
@@ -1,13 +1,12 @@
 @echo off
 set ECHO=
 
-set TARGET=%*
+set TARGET=Mjollnir
 set TARGET_DIR=%~dp0
+set TARGET_VERSION=5.0.9
 
 set MSBUILD=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 set NUGET=%TARGET_DIR%.nuget\NuGet.exe
 
-for %%I in (%TARGET%) do (
-	%ECHO% "%MSBUILD%" "%TARGET_DIR%%TARGET%.sln" /t:Rebuild /p:Configuration=Release
-	%ECHO% "%NUGET%" pack "%TARGET_DIR%%%I\%%I.nuspec" -OutputDir "%TARGET_DIR%%%I"
-)
+%ECHO% "%MSBUILD%" "%TARGET_DIR%%TARGET%.sln" /t:Rebuild /p:Configuration=Release
+%ECHO% "%NUGET%" pack "%TARGET_DIR%%TARGET%\%TARGET%.nuspec" -Properties version=%TARGET_VERSION% -OutputDir "%TARGET_DIR%%TARGET%"
--- NuPush.bat
+++ NuPush.bat
@@ -1,15 +1,13 @@
 @echo off
 set ECHO=
 
-set TARGET=%*
+set TARGET=Mjollnir
 set TARGET_DIR=%~dp0
 
 set NUGET=%TARGET_DIR%.nuget\NuGet.exe
 
-for %%I in (%TARGET%) do (
-	for %%J in ("%TARGET_DIR%%%I\*.nupkg") do (
-		set TARGET_PACKAGE=%%J
-	)
-
-	%ECHO% "%NUGET%" push "%TARGET_PACKAGE%"
+for %%I in ("%TARGET_DIR%%TARGET%\*.nupkg") do (
+	set TARGET_PACKAGE=%%I
 )
+
+%ECHO% "%NUGET%" push "%TARGET_PACKAGE%"

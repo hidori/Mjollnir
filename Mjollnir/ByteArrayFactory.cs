27a208af8dc4541c8fb6e0c4008e54b3b02852c9
Hiroaki SHIBUKI <Hiroaki.SHIBUKI@nifty.com>
2015-01-18 09:24:09 +0900

    Addred ByteArrayFactory

---
+++ Mjollnir/ByteArrayFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Mjollnir
+{
+    public static class ByteArrayFactory
+    {
+        public static byte[] Create(Action<Stream> action)
+        {
+            Throw.IfNull(action, "action");
+
+            using (var stream = new MemoryStream())
+            {
+                action(stream);
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
